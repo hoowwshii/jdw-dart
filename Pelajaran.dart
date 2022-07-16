eae23a99aa0c974dd9ac6f23c8b7d6afe572e8c9
ho(ly)shi et <68774737+hoowwshii@users.noreply.github.com>
2022-07-16 17:11:01 +0700

    ulang

---
+++ Pelajaran.dart
@@ -0,0 +1,15 @@
+class Pelajaran{
+
+
+String ?namapelajaran;
+  String ?token;
+  String ?url;
+
+  Pelajaran(String ?namapelajaran, String ?tkn, String ?url){
+    this.namapelajaran = namapelajaran;
+    this.token = tkn;
+    this.url = url;
+
+  }
+
+}
